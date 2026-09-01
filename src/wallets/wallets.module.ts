443b0a0f84006bc60c179034a219ab0f5cc1b071
Luis Calegari <lcalegari97@gmail.com>
2026-09-01 17:35:29 -0300

    feat(wallets): implement YieldsService with TNA calculation, UTC daily idempotency, and cron job

--- src/wallets/wallets.module.ts
+++ src/wallets/wallets.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { WalletsService } from './wallets.service';
 import { WalletsController } from './wallets.controller';
 import { WalletFactory } from './factories';
+import { YieldsService } from './services/yields.service';
 
 @Module({
   controllers: [WalletsController],
-  providers: [WalletsService, WalletFactory],
-  exports: [WalletsService, WalletFactory],
+  providers: [WalletsService, WalletFactory, YieldsService],
+  exports: [WalletsService, WalletFactory, YieldsService],
 })
 export class WalletsModule {}
